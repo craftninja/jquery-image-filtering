817328af02768ace24b483ffae0dcc53ff9806e5
EmilyPlatzer <emilyplatzer@gmail.com>
2015-05-06 11:01:25 -0600

    Iteration zero - poking around

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 	console.log("The document is ready!");
+	$( ".light" ).on( "click", function() {
+  	$( 'body' ).css( "background", "#000 url(img/tileable_wood_texture.png) repeat" );
+	});
+	$( ".dark" ).on( "click", function() {
+  	$( 'body' ).css( "background", "#000 url(img/dark_wood.png) repeat" );
+	});
+	$( ".forest" ).on( "click", function() {
+  	$( 'body' ).css( "background", "#000 url(img/forest.jpg) repeat" );
+  	$( 'body' ).css( "background-size", "cover" );
+	});
+
 })
